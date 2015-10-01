59a158b78260ca54de1fadd4623de0174bcbf5b7
alvaro garcia <derecerca@gmail.com>
2015-10-01 23:53:47 +0200

    add info on partial

--- selected.clj
+++ selected.clj
@@ -7,4 +7,17 @@
 #(reduce + [% %])
 #(+ % %)
 #(* 2 %)
+
 #(partial * 2)
+
+; applied to the right
+boot.user=> ((partial / 1) 2 3)
+; 1/6
+boot.user=> ((partial / 1) 2 3 4)
+; 1/24
+boot.user=> ((partial / 1) 2 3 4 5)
+; 1/120
+boot.user=> ((partial / 1) 2 3 4 5 6)
+; 1/720
+
+
